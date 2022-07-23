270c22ec53ce62aace6bb1dc84f0d352a4814dfe
Michal Bendik <108522291+bendikMichal@users.noreply.github.com>
2022-07-23 14:12:48 +0200

    Fixed a bug with rectangle-circle collision detection

--- colLib.py
+++ colLib.py
@@ -14,14 +14,15 @@ pygame.init()
 
 
 def collideCircle(c1, c2):
-    return sqrt((c1[0] - c2[0]) ** 2 +(c1[1] - c2[1]) ** 2) <= c1[2] + c2[2]
+    return sqrt((c1[0] - c2[0]) ** 2 +(c1[1] - c2[1]) ** 2) < c1[2] + c2[2]
 
 
 def colCircleRect(c, r):
     cr = pygame.Rect(c[0] - c[2], c[1] - c[2], c[2] * 2, c[2] * 2)
-    rc = [r.x + r.width / 2, r.y + r.height / 2, r.width/2]
+    
+    rc = [r.x + r.width / 2, r.y + r.height / 2, sqrt((r.x - (r.x + r.width / 2)) ** 2 +(r.y - (r.y + r.height / 2)) ** 2)]
     
     return (cr.colliderect(r)) and (collideCircle(c, rc))
-     
+
 
 
